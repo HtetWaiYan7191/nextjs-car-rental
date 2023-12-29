1e8e7ca9776d16ac7d593e58e909445aa5491527
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-29 16:16:25 +0630

    Implement manufacturers array

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import { Hero } from '@/components'
+import CarCatalogue from '@/components/CarCatalogue'
 
 export default function Home() {
   return (
     <main className="oveflow-hidden">      
       <Hero/>
+      <CarCatalogue/>
     </main>
   )
 }
