308ec7f03e11672abe9ed0fd23d467c095ed5b0a
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-01-02 13:26:13 +0630

    Modify the custom button component / update props and add right icon

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,14 +1,22 @@
 import { CustomButtonProps } from '@/types'
+import Image from 'next/image'
 import React from 'react'
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, textStyles, rightIcon, handleClick}: CustomButtonProps) => {
   return (
     <button className={`${containerStyles}`}
     disabled={false}
     type={'button'}
     onClick={handleClick}
     >
-        {title}
+        <span className={`flex-1 ${textStyles}`}>
+          {title}
+        </span>
+        {rightIcon && (
+          <div className='relative w-7 h-7 mr-[1rem]'>
+              <Image src={rightIcon} alt='right Icon' fill className=' object-contain right-arrow'/>
+          </div>
+        )}
     </button>
   )
 }
