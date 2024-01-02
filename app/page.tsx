62e984e5330098842f6af686dacd56d866f0062b
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-01-02 13:25:07 +0630

    Get api in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import { Hero } from '@/components'
 import CarCatalogue from '@/components/CarCatalogue'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="oveflow-hidden">      
       <Hero/>
-      <CarCatalogue/>
+      <CarCatalogue isDataEmpty = {isDataEmpty} allCars = {allCars}/>
     </main>
   )
 }
