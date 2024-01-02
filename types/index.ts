6f2a3dd0e2f34cb91742f005c765fa1687aeaa10
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-01-02 13:24:55 +0630

    Add new type for components

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,13 @@
+import { CarCardProps } from '@/types';
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
+    btnType?: 'button' | 'submit';
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
@@ -19,3 +24,29 @@ export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer: string) => void;
 }
+
+export interface Car  {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+};
+
+export interface CarCardProps {
+    car: Car;
+}
+
+export interface CarCatalogueProps {
+    isDataEmpty: boolean;
+    allCars: Car[];
+}
+
+
