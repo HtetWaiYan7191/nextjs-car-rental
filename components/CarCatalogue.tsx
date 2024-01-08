bff2ff791964205585b5549a8d99c0fef3ed6abb
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-01-08 10:48:43 +0630

    Implement custom filters using headlessui react

--- components/CarCatalogue.tsx
+++ components/CarCatalogue.tsx
@@ -3,8 +3,10 @@ import SearchBar from './SearchBar'
 import CustomFilter from './CustomFilter'
 import { CarCatalogueProps } from '@/types'
 import CarCard from './CarCard'
+import { fuels, yearsOfProduction } from '@/constants'
 
 const CarCatalogue = ({isDataEmpty, allCars} : CarCatalogueProps) => {
+  const years = [...yearsOfProduction];
   return (
     <section className='car-catalogue-section mt-12 px-10' id='discover'>
         <h2 className='font-semibold text-neutral-800 text-2xl'>Car Catalogue</h2>
@@ -13,8 +15,8 @@ const CarCatalogue = ({isDataEmpty, allCars} : CarCatalogueProps) => {
         <div className='home-filters my-5 flex gap-y-5 flex-col md:flex-row justify-between'>
             <SearchBar/>
             <div className='flex gap-5'>
-            <CustomFilter title="fuel"/>
-            <CustomFilter title="year"/>
+            <CustomFilter title="fuel" options={fuels}/>
+            <CustomFilter title="year" options={years}/>
         </div>
         </div>
 
--- types/index.ts
+++ types/index.ts
@@ -15,8 +15,14 @@ export interface MobileNavbarProps {
     showMenu?: boolean;
 }
 
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
 export interface CustomFilterProps {
     title: string;
+    options: OptionProps[];
 }
 
 export interface SearchManufacturerProps {
